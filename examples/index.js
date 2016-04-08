b995ac74ed537b92d29810516ae02de59c50bc30
Sergey Nanovsky <sergey.nanovsky@gmail.com>
2016-04-08 00:15:04 -0700

    examples

---
+++ examples/index.js
@@ -0,0 +1,35 @@
+(function () {
+  var sandbox = {
+    print: function (message) {
+      console.log('sum = ' + message);
+    }
+  };
+
+  var code1 = 'function sum(a, b) { return a + b; } sum(5, 10);';
+  var code2 = 'function sum(a, b) { return a + b; } print(sum(5, 10));';
+
+  var context = vm.createContext(sandbox);
+
+  var script1 = new vm.Script(code1);
+  var script2 = new vm.Script(code2);
+
+  console.log('Script.runInNewContext');
+  console.log(script1.runInNewContext());
+
+  console.log('vm.runInNewContext');
+  console.log(vm.runInNewContext(code1));
+
+  console.log('Script.runInContext');
+  script2.runInContext(context);
+
+  console.log('vm.runInContext');
+  vm.runInContext(code2, context);
+
+  window.print = sandbox.print;
+
+  console.log('Script.runInThisContext');
+  script2.runInThisContext();
+
+  console.log('vm.runInThisContext');
+  vm.runInThisContext(code2);
+}());
