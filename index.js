64876f5a1e6b7bef12c14b82c9ec208aa408d1e6
Sergey Nanovsky <sergey.nanovsky@gmail.com>
2016-04-07 23:27:04 -0700

    run code in new context

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 (function () {
   var vm = {};
 
+  function createIFrame() {
+    var iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    return iframe;
+  }
+
+  function runCodeInNewContext(code, sandbox) {
+    var iframe = createIFrame();
+    var result;
+    document.body.appendChild(iframe);
+    if (sandbox) {
+      Object.keys(sandbox).forEach(function (key) {
+        iframe.contentWindow[key] = sandbox[key];
+      });
+    }
+    result = iframe.contentWindow.eval(code);
+    document.body.removeChild(iframe);
+    return result;
+  }
+
   function Script(code) {
     this.code = code;
   }
@@ -9,6 +29,7 @@
   };
 
   Script.prototype.runInNewContext = function (sandbox) {
+    return runCodeInNewContext(this.code, sandbox);
   };
 
   Script.prototype.runInThisContext = function () {
@@ -22,13 +43,14 @@
   vm.isContext = function (sandbox) {
   };
 
-  vm.runInContext = function (code, sandbox) {
+  vm.runInContext = function (code, context) {
   };
 
   vm.runInDebugContext = function (code) {
   };
 
   vm.runInNewContext = function (code, sandbox) {
+    return runCodeInNewContext(code, sandbox);
   };
 
   vm.runInThisContext = function (code) {
